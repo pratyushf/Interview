f5a9b338fe875c32c4e1937a80b431641c363041
Pratyush Fekarikar <pratyush.f@venhan.com>
2025-03-21 17:05:09 +0000

    www

--- Interview/src/App.jsx
+++ Interview/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className=" bg-green-500 text-2xl">heyyyy</div>;
+  return <div className=" bg-green-500 text-2xl">hey</div>;
 }
 
 export default App;
